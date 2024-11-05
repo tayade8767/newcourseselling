afdcbdb9861b4c58e1a0ba19f0a4ba9af1396d79
akash <akashtayade5668@gmail.com>
2024-11-05 16:40:05 +0530

    new changes commited

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState, useRef } from 'react';
 import Typewriter from "./Typewriter";
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,16 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 // import SearchBox from './SearchBox';
 
 function NavBar() {
   return (
-    <div className="h-16 fixed top-0 w-full bg-white flex items-center justify-between px-4 md:px-8 shadow-md z-50 bg-amber-700">
+    <div className="h-16 fixed top-0 w-full bg-white flex items-center justify-between px-4 md:px-8 shadow-md z-20">
       <div className="flex items-center">
         <div className="p-1 bg-white rounded-full">
           <img
             src="https://i.insider.com/63d93b280a08ae0018a62b4f?width=700"
             alt="Profile"
-            className="h-10 w-10 rounded-full"
+            className="h-[3.5rem] w-[3.5rem] rounded-full"
           />
         </div>
       </div>
@@ -42,11 +43,11 @@ function NavBar() {
         </div>
 
         
-        <div className="p-1 bg-white rounded-full">
+        <div className=" bg-red-500 rounded-full">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNWvRBKQ78rfLVw4-qlKDl0NTS7pNcf0xVJ-xljgMfevoEujQQ9T5RUrVFcfPwcUlJxjo&usqp=CAU"
             alt="Profile"
-            className="h-10 w-10 rounded-full"
+            className="h-11 w-11 rounded-full"
           />
         </div>
       </div>
